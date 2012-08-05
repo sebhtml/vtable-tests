38fde1e7531b398a87ef3538d30dc7d9d24b3801
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-08-04 23:12:03 -0400

    I added results for Xeon processor (X5560).
    Signed-off-by: Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 grep "model name" /proc/cpuinfo|head -n1
+gcc --version|head -n1
 
 echo ""
 echo "C"
+echo ""
 gcc test.c -O3 -Wall
 time ./a.out
 
 echo ""
 echo "C++"
+echo ""
 g++ test.cpp -O3 -Wall
 time ./a.out
 
